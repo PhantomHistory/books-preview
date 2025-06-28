4708adc18d0c2b39b12f6915d39048e661c5e7a0
PhantomHistory <titto.99.pp@gmail.com>
2025-06-28 16:06:59 +0200

    FIX : zoom level based on internet quality

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
@@ -6,15 +7,35 @@ export default function BookCard({ book }) {
   const info = book.volumeInfo;
   const accessInfo = book.accessInfo;
 
+  const [zoomLevel, setZoomLevel] = useState(2);
+
+  useEffect(() => {
+    const connection =
+      navigator.connection || navigator.mozConnection || navigator.webkitConnection;
+
+    if (connection) {
+      const type = connection.effectiveType || "4g";
+      if (type === "5g") {
+        setZoomLevel(3);
+        console.log(zoomLevel,'zoom 3')
+      } else {
+        setZoomLevel(2);
+                console.log(zoomLevel,'zoom 2 ')
+
+      }
+    }
+  }, []);
+
   const truncateText = (text, maxLength) => {
     if (!text) return "";
     if (text.length <= maxLength) return text;
     return text.slice(0, maxLength) + "...";
   };
 
+  // Sostituisci zoom con zoomLevel dinamico
   const cleanThumbnailUrl = info.imageLinks?.thumbnail?.replace(
-    /([&?])zoom=1(&|$)/,
-    (match, p1, p2) => `${p1}zoom=2${p2 === "&" ? "&" : ""}`
+    /([&?])zoom=\d+(&|$)/,
+    (match, p1, p2) => `${p1}zoom=${zoomLevel}${p2 === "&" ? "&" : ""}`
   );
 
   const previewHref =
@@ -22,7 +43,6 @@ export default function BookCard({ book }) {
       ? accessInfo.webReaderLink
       : info.previewLink;
 
-  // Funzione per aprire il link in una nuova scheda
   const handleCardClick = () => {
     if (previewHref) {
       window.open(previewHref, "_blank", "noopener,noreferrer");
@@ -40,6 +60,7 @@ export default function BookCard({ book }) {
           src={cleanThumbnailUrl}
           alt={info.title}
           className="object-fit-contain"
+          loading="lazy" // lazy loading nativo
         />
       )}
       <Card.Body>
